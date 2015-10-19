d47e260064742ec688f91a9916235a218ed5dbe6
zhulinpinyu <mulixiang199016@gmail.com>
2015-10-19 10:32:15 +0800

    when post create and triger publish to RabbitMQ

--- blog/app/controllers/posts_controller.rb
+++ blog/app/controllers/posts_controller.rb
@@ -28,6 +28,7 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        Publisher.publish("posts", @post.attributes)
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
         format.json { render :show, status: :created, location: @post }
       else
